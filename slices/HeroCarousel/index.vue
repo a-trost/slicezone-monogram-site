d783b141ed81f65f80d2220087bcc250dccf0be5
Alex Trost <alexrtrost@gmail.com>
2021-04-29 12:20:11 -0400

    Add Hero Carousel

---
+++ slices/HeroCarousel/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="flex hero-carousel">
+   <prismic-image class="w-full object-cover" :field="slice.items[0].image" />
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style lang="css">
+ @screen md{
+  .hero-carousel{
+    height: 675px;
+  }
+ }
+
+   .hero-carousel{
+    height: 337px;
+  }
+</style>
