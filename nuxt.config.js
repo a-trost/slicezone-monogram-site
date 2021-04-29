3b428fddbd5a9282f265ccb6f37b068f2fe9d690
Alex Trost <alexrtrost@gmail.com>
2021-04-29 11:28:51 -0400

    Configure tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,9 +29,9 @@ export default {
     }]
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["vue-essential-slices/src/styles/styles.scss"],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
+  tailwindcss: {jit:true},
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      navy: '#1a2456',
+      orange: '#ff501c',
+      black: '#080b1b',
+      peach: '#ff8b68',
+      offWhite: '#f0efed',
+    },
+    fontFamily: {
+      // sans: ['Graphik', 'sans-serif'],
+      // serif: ['Merriweather', 'serif'],
+    },
+  },
+}
